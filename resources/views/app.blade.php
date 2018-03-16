b95640564a3902d3cba7d266ced5fa83260ff034
David Chioqueti <davidchioqueti@gmail.com>
2018-03-16 17:58:36 -0300

    primeiro commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Meu projeto 1</title>
+
+
+    <meta name="csrf-token" content="{{csrf_token()}}">
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    
+<div class="content">
+<div id="app">
+<example></example>
+</div>
+</div>
+
+<script type='text/javascript' src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
